f0b64ba2479f6608849bbb2eaa6496fc301be104
mitrank <mitrankshah@yahoo.com>
2024-12-06 02:10:30 +0530

    created account form with name field

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -6,9 +6,21 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { useNewAccount } from "../hooks/use-new-account";
+import { AccountForm } from "./account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+
+  const handleOnSubmit = (values: FormValues) => {
+    console.log("submit values", values);
+  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
@@ -19,6 +31,11 @@ export const NewAccountSheet = () => {
             Create a new account to track your transactions.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={handleOnSubmit}
+          disabled={false}
+          defaultValues={{ name: "" }}
+        />
       </SheetContent>
     </Sheet>
   );
