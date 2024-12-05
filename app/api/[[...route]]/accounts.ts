9861c5f6ab37f7712f7531f7d94797a443d26d27
mitrank <mitrankshah@yahoo.com>
2024-12-05 22:42:13 +0530

    1. added hono lib 2. added query-provider 3. created custom hook to get accounts

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,14 +1,23 @@
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 import { Hono } from "hono";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { eq } from "drizzle-orm";
 
-const app = new Hono().get("/", async (c) => {
+const app = new Hono().get("/", clerkMiddleware(), async (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json({ error: "Unauthorized" }, 401);
+  }
+
   const data = await db
     .select({
       id: accounts.id,
       name: accounts.name,
     })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
 
   return c.json({ data });
 });
