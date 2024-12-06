b6272e956064a763f1dce6b0e2c967e6133d0e66
mitrank <mitrankshah@yahoo.com>
2024-12-06 15:36:53 +0530

    added basic add account card

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Plus } from "lucide-react";
+
 const AccountsPage = () => {
-  return <div>Accounts Page</div>;
+  const newAccount = useNewAccount();
+
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Accounts Page</CardTitle>
+          <Button size="sm" onClick={newAccount.onOpen}>
+            <Plus className="size-4 mr-2" />
+            Add New
+          </Button>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 };
 
 export default AccountsPage;
