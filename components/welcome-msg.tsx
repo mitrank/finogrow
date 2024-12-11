f9aad09b76e69a17648aa2e27b04b3f895c360d0
mitrank <mitrankshah@yahoo.com>
2024-12-11 23:34:45 +0530

    use isSignedIn for proper user loading

--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -3,13 +3,13 @@
 import { useUser } from "@clerk/nextjs";
 
 export const WelcomeMessage = () => {
-  const { user, isLoaded } = useUser();
+  const { user, isSignedIn } = useUser();
 
   return (
     <div className="space-y-2 mb-4">
       <h2 className="text-2xl lg:text-4xl text-white font-medium">
         Welcome back
-        {isLoaded ? `, ${user?.firstName || user?.username}👋` : " "}
+        {isSignedIn ? `, ${user?.firstName || user?.username}👋` : " "}
       </h2>
       <p className="text-sm lg:text-base text-[#89B6FD]">
         This is your Financial Overview Report
